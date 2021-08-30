77a7c7043012945f85c1248c17987be3c0cbaa08
He Bai <he.bai@uwaterloo.ca>
2021-08-30 14:40:59 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ docker build -t pytorch:gpu -f Dockerfile .
 
 docker rmi $(docker images -f "dangling=true" -q)
 
-docker container run -itd  --name h32bai_lastest  --mount type=bind,source=/data/h32bai/projects/,target=/root/projects --mount type=bind,source=/data/h32bai/datasets/,target=/root/datasets --mount type=bind,source=/data/h32bai/checkpoints/,target=/root/checkpoints   --shm-size=16g richardbaihe/pytorch:lastest /bin/zsh
+docker container run -itd  --name h32bai_lastest --gpus=all --mount type=bind,source=/data/h32bai/projects/,target=/root/projects --mount type=bind,source=/data/h32bai/datasets/,target=/root/datasets --mount type=bind,source=/data/h32bai/checkpoints/,target=/root/checkpoints   --shm-size=16g richardbaihe/pytorch:lastest /bin/zsh
 
 docker inspect -f "{{ .NetworkSettings.IPAddress }}" Container_Name
